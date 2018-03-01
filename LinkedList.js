d8370e6a15a99d8ed10fc6f5085ea2fddcf1790d
Sam Gould <sjg326@gmail.com>
2018-03-01 12:30:27 -0800

    supplemental functions for linked list completed

--- LinkedList.js
+++ LinkedList.js
@@ -9,10 +9,10 @@ class LinkedList {
 
   insertFirst(item) {
     if (this.head === null) {
-      console.log('head is null');
+
       this.head = new _Node(item, null);
     } else {
-      console.log('head is not null');
+
       const currentPointer = this.head;
       this.head = new _Node(item, currentPointer);
     }
@@ -20,15 +20,15 @@ class LinkedList {
 
   insertLast(item) {
     if (this.head === null) {
-      console.log('head is null insertLast');
+
       this.head = new _Node(item, null);
     } else {
       let currentNode = this.head;
       while (currentNode.next !== null) {
-        console.log('once through the while loop');
+  
         currentNode = currentNode.next;
       }
-      console.log('setting the new node at', currentNode);
+
       currentNode.next = new _Node(item, null);
     }
   }
@@ -54,8 +54,20 @@ class LinkedList {
     previousNode.next = new _Node(newItem, currentNode);
   }
 
-  insertAfter(){
-
+  insertAfter(newItem, targetItem){
+    if (this.head === null) {
+      throw new Error('Item not found');
+    }
+    let currentNode = this.head;
+    let nextNode = currentNode.next; 
+    while (currentNode.value !== targetItem){
+      currentNode = nextNode;
+      nextNode = currentNode.next;
+      if (currentNode === null){
+        throw new Error('An error has occured :)');
+      }
+    }
+    currentNode.next = new _Node(newItem, nextNode);
   }
 
   insertAt(newItem, position) {
@@ -68,7 +80,7 @@ class LinkedList {
     }
     let currentNode = this.head;
     let previousNode;
-    // initialize at 0 to loop through to the end node
+    // initialize at 1 to loop through to the end node
     for (let i = 1; i < position; i++) {
       previousNode = currentNode;
       currentNode = currentNode.next;
@@ -97,7 +109,6 @@ class LinkedList {
         throw new Error('This item does not exist in this list. You cannot remove what was never there.');
       } 
     }
-    console.log(currentNode, 'was removed');
     previousNode.next = currentNode.next;
   }
 
@@ -116,6 +127,66 @@ class LinkedList {
   }
 }
 
+const display = list => {
+  const results = [];
+  let currentNode = list.head;
+  while (currentNode !== null) {
+    results.push(currentNode);
+    currentNode = currentNode.next;
+  }
+  return results;
+};
+
+const size = list => {
+  let counter = 0;
+  let currentNode = list.head;
+  while (currentNode !== null) {
+    currentNode = currentNode.next;
+    counter++;
+  }
+  return counter;
+};
+
+const isEmpty = list => {
+  if (!list) {
+    throw new Error('List not found');
+  }
+  if (list.head === null) {
+    return true;
+  }
+  return false;
+}
+
+const findPrevious = (list, item) => {
+  if (list.head === null){
+    throw new Error('Item not found');
+  }
+  if (list.head.value === item) {
+    throw new Error('Something has gone wrong');
+  }
+  let currentNode = list.head;
+  let previousNode;
+  while (currentNode.value !== item) {
+    previousNode = currentNode;
+    currentNode = currentNode.next;
+    if (currentNode === null) {
+      throw new Error('This item is not included in this list');
+    }
+  }
+  return previousNode;
+}
+
+const findLast = list => {
+  if (list.head === null){
+    throw new Error('The list is empty');
+  }
+  let currentNode = list.head;
+  while (currentNode.next !== null) {
+    currentNode = currentNode.next;
+  }
+  return currentNode;
+}
+
 const main = () => {
   const SLL = new LinkedList();
   SLL.insertFirst('Apollo');
@@ -125,6 +196,14 @@ const main = () => {
   SLL.insertFirst('Starbuck');
   SLL.insertLast('Tauhida');
   SLL.insertBefore('Athena', 'Boomer');
+  SLL.insertAfter('Hot Dog', 'Helo');
+  SLL.insertAt('Kat', 3);
+  SLL.remove('Tauhida');
+  // console.log(display(SLL));
+  // console.log(size(SLL));
+  // console.log(isEmpty(SLL));
+  // console.log(findPrevious(SLL, 'Kat'));
+  // console.log(findLast(SLL));
 };
 
 main();
