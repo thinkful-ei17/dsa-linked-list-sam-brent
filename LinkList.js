5a7565cccf3ce14f24c5be4469273fc2767dce2e
Brent Guistwite <brentguistwite@gmail.com>
2018-03-01 11:47:50 -0500

    stub out methods for linkedlist class

---
+++ LinkList.js
@@ -0,0 +1,24 @@
+const _Node = require('./_Node.js');
+
+const _node = new _Node();
+
+class LinkedList {
+  constructor(){
+    this.head = null;
+  }
+
+  insertFirst() {
+
+  }
+
+  insertLast() {
+
+  }
+
+  remove() {
+
+  }
+  find() {
+
+  }
+}
