c721aeb3c38268edf06fee01a6901051f8368215
Sam Gould <sjg326@gmail.com>
2018-03-01 09:50:25 -0800

    completed coding core functionality for linked list from scratch

--- LinkedList.js
+++ LinkedList.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const _Node = require('./_Node.js');
 
 class LinkedList {
@@ -16,23 +18,56 @@ class LinkedList {
     }
   }
 
-  insertLast() {
-
+  insertLast(item) {
+    if (this.head === null) {
+      console.log('head is null insertLast');
+      this.head = new _Node(item, null);
+    } else {
+      let currentNode = this.head;
+      while (currentNode.next !== null) {
+        console.log('once through the while loop');
+        currentNode = currentNode.next;
+      }
+      console.log('setting the new node at', currentNode);
+      currentNode.next = new _Node(item, null);
+    }
   }
 
-  remove() {
-
+  remove(item) {
+    let currentNode = this.head;
+    let previousNode;
+    while (currentNode.value !== item) {
+      previousNode = currentNode;
+      currentNode = currentNode.next;
+      if (currentNode === null) {
+        throw new Error('This item does not exist in this list. You cannot remove what was never there.');
+      } 
+    }
+    console.log(currentNode, 'was removed');
+    previousNode.next = currentNode.next;
   }
-  find() {
 
+  find(item) {
+    let currentNode = this.head;
+    while (currentNode.value !== item) {
+      currentNode = currentNode.next;
+      if (currentNode === null) {
+        throw new Error('This item is not included in this list');
+      }
+    }
+    return currentNode.value;
   }
 }
 
 const main = () => {
-  const test = new LinkedList();
-  console.log(test.insertFirst(3));
-  console.log(test.insertFirst(100));
-
-}
+  const SLL = new LinkedList();
+  SLL.insertFirst('Apollo');
+  SLL.insertFirst('Boomer');
+  SLL.insertFirst('Helo');
+  SLL.insertFirst('Husker');
+  SLL.insertFirst('Starbuck');
+  SLL.insertLast('Tauhida');
+  SLL.remove('squirrel');
+};
 
 main();
